6927b6d115f143a7a403782a83f2351f37c37699
AdrianBegic123 <hej12351@gmail.com>
2025-05-14 21:00:59 +0200

    medvetet fel i testet

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,10 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('pop should remove the top element from the stack', () => {
+    stack.push("äpple");
+    stack.push("banan");
+    stack.pop();
+    expect(stack.peek()).toBe("apelsin");
+});
